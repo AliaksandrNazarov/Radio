09ff18cf9fc57fb735a9093a409d99867ca13de6
Alexander Nazarov <alexander.nazarov@kaltura.com>
2022-05-14 14:50:59 +0300

    path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        root: ['./'],
+        alias: {
+          '@src': './src',
+          '@api': './src/api',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
