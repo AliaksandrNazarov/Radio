b7e87a271f2f5375feb603c047fe2021fe5ec195
Alexander Nazarov <alexander.nazarov@kaltura.com>
2022-05-16 14:34:47 +0300

    App component to components folder

---
+++ src/components/App/App.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View } from 'react-native';
+import { fetchRadioData, IRadioStation } from '@api';
+import Accordion from 'react-native-collapsible/Accordion';
+import { RadioListItem, RadioDetails, RadioHeader, RadioFooter } from '@components';
+import { styles } from './styles';
+
+const App = () => {
+  const [stationsData, setStationsData] = useState<IRadioStation[]>([]);
+
+  const [activeSections, setActiveSections] = useState<number[]>([]);
+  const [selectedSection, setSelectedSection] = useState<number[]>([]);
+
+  useEffect(() => {
+    try {
+      fetchRadioData(setStationsData);
+    } catch (err) {
+      // TODO: handle error somehow
+    }
+  }, []);
+
+  const updateSections = (activeSection: number[]) => {
+    setActiveSections(activeSection);
+    setSelectedSection(activeSection);
+  };
+
+  const turnOffRadio = () => updateSections([]);
+
+  const selectedSectionItem: IRadioStation = stationsData.length
+    ? stationsData[selectedSection[0]]
+    : ({} as IRadioStation);
+
+  const renderContent = () => (
+    <RadioDetails
+      stationsData={stationsData}
+      selectedSection={selectedSection}
+      setSelectedSection={setSelectedSection}
+    />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <RadioHeader onPowerPress={turnOffRadio} />
+      <View style={styles.containerAccordeonFooter}>
+        <Accordion
+          activeSections={activeSections}
+          sections={stationsData}
+          renderHeader={RadioListItem}
+          renderContent={renderContent}
+          onChange={updateSections}
+          renderAsFlatList
+        />
+        <RadioFooter selectedSectionItem={selectedSectionItem} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export { App };
