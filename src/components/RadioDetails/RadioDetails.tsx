17e9a875d4af4d305f3c45dea0b073c3af3bcc0b
Alexander Nazarov <alexander.nazarov@kaltura.com>
2022-05-16 10:37:12 +0300

    switching logic for RadioDetails

--- src/components/RadioDetails/RadioDetails.tsx
+++ src/components/RadioDetails/RadioDetails.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import { IRadioStation } from '@api';
 import { View, Image } from 'react-native';
 import { styles } from './styles';
+import { CircleButton } from './CircleButton';
 
-const RadioDetails = ({ image }: IRadioStation) => {
+interface IProps {
+  stationsData: IRadioStation[];
+  selectedSection: number[];
+  setSelectedSection: (position: number[]) => void;
+}
+
+const RadioDetails = ({ selectedSection, stationsData, setSelectedSection }: IProps) => {
+  const item = stationsData[selectedSection[0]];
+
+  const onPlusPress = () => {
+    const nextPlusValue = selectedSection[0] + 1;
+    const valueToSet = nextPlusValue > stationsData.length - 1 ? 0 : nextPlusValue;
+    setSelectedSection([valueToSet]);
+  };
+
+  const onMinusPress = () => {
+    const nextMinusValue = selectedSection[0] - 1;
+    const valueToSet = nextMinusValue < 0 ? stationsData.length - 1 : nextMinusValue;
+    setSelectedSection([valueToSet]);
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={{ uri: image }} style={styles.image} />
+      <CircleButton plus={false} onPress={onMinusPress} />
+      <Image source={{ uri: item?.image }} style={styles.image} />
+      <CircleButton onPress={onPlusPress} />
     </View>
   );
 };
